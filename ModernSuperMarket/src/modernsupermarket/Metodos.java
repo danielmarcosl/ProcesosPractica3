11eaed05f135404df4e61bae155a2934ebf7e041
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-12-04 17:50:48 +0100

    Created methods class

---
+++ ModernSuperMarket/src/modernsupermarket/Metodos.java
@@ -0,0 +1,60 @@
+package modernsupermarket;
+
+import java.awt.TextArea;
+import java.util.Random;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ * @version 1 01/12/2015
+ */
+public class Metodos {
+
+    public static void generarTextFields(int num) {
+        for (int i = 0; i < num; i++) {
+            Ventana.texto_ventana.set(i, new TextArea());
+        }
+    }
+
+    /**
+     * Metodo para inicializar los estados de las cajas
+     */
+    public static void inicializarCajas() {
+        for (int i = 0; i < ModernSuperMarket.cajas; i++) {
+            ModernSuperMarket.estadoCaja[i] = true;
+        }
+    }
+
+    /**
+     * Metodo para crear un numero aleatorio entre 50 y 500 para los clientes
+     *
+     * @return Numero generado
+     */
+    public static int calcularClientes() {
+        Random rand = new Random();
+        int num = rand.nextInt(450) + 50;
+        return num;
+    }
+
+    /**
+     * Metodo para crear un numero aleatorio entre 1 y 6 para las cajas
+     *
+     * @return Numero generado
+     */
+    public static int calcularCajas() {
+        Random rand = new Random();
+        int num = rand.nextInt(6) + 4;
+        return num;
+    }
+
+    /**
+     * Metodo para crear un numero aleatorio entre 1 y 100 para el pago
+     *
+     * @return Numero generado
+     */
+    public static int generarPago() {
+        Random r = new Random();
+        int num = r.nextInt(100) + 1;
+        return num;
+    }
+}
