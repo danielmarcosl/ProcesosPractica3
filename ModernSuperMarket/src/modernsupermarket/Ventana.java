08bde147fd6c13e06b8f17fa4db8f18c5700f948
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-12-04 19:46:17 +0100

    Update windows form

--- ModernSuperMarket/src/modernsupermarket/Ventana.java
+++ ModernSuperMarket/src/modernsupermarket/Ventana.java
@@ -1,8 +1,9 @@
 package modernsupermarket;
 
-import java.awt.TextArea;
+import java.awt.Color;
 import java.util.ArrayList;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -11,26 +12,72 @@ import javax.swing.JFrame;
  */
 public class Ventana {
 
+    // Ventana
     private JFrame ventana = new JFrame();
-    public static ArrayList<TextArea> texto_ventana;
+    // Array de paneles para cada caja
+    public static ArrayList<JTextArea> texto_ventana;
+    // Panel de informacion 
+    public static JTextArea info = new JTextArea();
+    // Nombre de la ventana
     private String nombre = "ModernSuperMarket";
 
     /**
-     * Creacion de ventana de Windows
+     * Constructor de ventana de Windows
      *
      * @param nombre Nombre de la ventana
      */
     Ventana(int num) {
+        // Asignacion de valores de la ventana
         ventana.setName(nombre);
         ventana.setTitle(nombre);
-        ventana.setSize(400, 400);
+        ventana.setSize((200 * num), 800); // Segun el numero de cajas varia el tamano
         ventana.setVisible(true);
 
-        for (int i = 0; i < num; i++) {
-            texto_ventana.set(i, new TextArea());
-        }
+        // Inicializacion del array de paneles
+        texto_ventana = new ArrayList<JTextArea>();
 
-        for (int i = 0; i < num; i++) {
+        for (int i = 0; i < (num + 1); i++) {
+
+            // Inicializacion de cada panel del array
+            texto_ventana.add(i, new JTextArea());
+
+            // Asignacion de valores a cada panel
+            switch (i) {
+                case 0:
+                    texto_ventana.get(i).setBounds(0, 0, 200, 700);
+                    texto_ventana.get(i).setBackground(Color.red);
+
+                    // Panel de informacion
+                    info.setBounds(0, 700, (200 * num), 100);
+                    info.setBackground(Color.gray);
+                    ventana.add(info);
+                    break;
+                case 1:
+                    texto_ventana.get(i).setBounds(200, 0, 200, 700);
+                    texto_ventana.get(i).setBackground(Color.green);
+                    break;
+                case 2:
+                    texto_ventana.get(i).setBounds(400, 0, 200, 700);
+                    texto_ventana.get(i).setBackground(Color.yellow);
+                    break;
+                case 3:
+                    texto_ventana.get(i).setBounds(600, 0, 200, 700);
+                    texto_ventana.get(i).setBackground(Color.pink);
+                    break;
+                case 4:
+                    texto_ventana.get(i).setBounds(800, 0, 200, 700);
+                    texto_ventana.get(i).setBackground(Color.cyan);
+                    break;
+                case 5:
+                    texto_ventana.get(i).setBounds(1000, 0, 200, 700);
+                    texto_ventana.get(i).setBackground(Color.blue);
+                    break;
+                case 6:
+                    texto_ventana.get(i).setBounds(0, 0, 0, 0);
+                    texto_ventana.get(i).setBackground(Color.gray);
+                    break;
+            }
+            // Anadimos en cada vuelta del bucle el panel correspondiente a la ventana
             ventana.add(texto_ventana.get(i));
         }
     }
